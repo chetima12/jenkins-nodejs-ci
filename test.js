aeb222c90f5b7b34d8cac1ae50c214153c9cb0a4
chetima <chetimahusseni@gmail.com>
2026-08-09 13:28:49 +0100

    Add a second test

--- test.js
+++ test.js
@@ -4,4 +4,9 @@ if (add(2, 3) !== 5) {
     throw new Error("Test failed");
 }
 
+if (add(10, 20) !== 30) {
+    throw new Error("Second addition test failed");
+}
+
+
 console.log("All tests passed!");
